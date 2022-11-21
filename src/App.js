a735feb9df103341d0ddd6e898fb2ce80f5736b5
Hank Kim <hank29206880@gmail.com>
2022-11-21 13:23:29 +0900

    export excel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { CSVLink } from "react-csv";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const ExcelDownload = ({ data }) => {
+  return (
+    <button>
+      <CSVLink data={data} filename="users.csv" target="_blank">
+        Export Excel
+      </CSVLink>
+    </button>
+  );
+};
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    const getData = async () => {
+      const response = await axios.get(
+        "https://cs-tool-api.seedn.info/table/slaveDevice?page=1&limit=10"
+      );
+      setData([...response.data.result]);
+    };
+    getData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ExcelDownload data={data} />
     </div>
   );
 }
